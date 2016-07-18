24c5214740267ed99af732234c634e80263fac54
marioluigiman <mikipux7@gmail.com>
2016-07-18 13:01:26 -0500

    First version

--- lib/image64.ex
+++ lib/image64.ex
@@ -1,2 +1,27 @@
 defmodule ImageBase64Handler do
+  ## Note that these are url encoding and decoding
+  import Base
+
+
+  def base64FileToImage(fileInputPath, fileOutputPath) do
+    {:ok, base64data} = File.read(fileInputPath)
+    base64ToImage(base64data, fileOutputPath)
+  end
+
+  def base64ToImage(base64data, fileOutputPath) do
+    imgdata = url_decode64!(base64data)
+    {:ok, file} = File.open(fileOutputPath, [:write])
+    IO.binwrite(file, imgdata)
+    File.close(file)
+    true
+  end
+
+  def imagetobase64url(fileInputPath, fileOutputPath) do
+    {:ok, imageData} = File.read(fileInputPath)
+    base64data = url_encode64(imageData)
+    {:ok, file} = File.open(fileOutputPath, [:write])
+    IO.binwrite(file, base64data)
+    File.close(file)
+  end
+
 end
--- test/image64_test.exs
+++ test/image64_test.exs
@@ -2,7 +2,16 @@ defmodule ImageBase64HandlerTest do
   use ExUnit.Case
   doctest ImageBase64Handler
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "simple image conversion" do
+    ImageBase64Handler.imagetobase64url("testfiles/birdie.png", "testbuild/test.base64")
+    ImageBase64Handler.base64FileToImage("testbuild/test.base64","testbuild/birdie.png")
+
+    {:ok, file1} = File.read("testbuild/birdie.png")
+    {:ok, file2} = File.read("testfiles/birdie.png")
+
+    assert file1 == file2
   end
+
+
+
 end
